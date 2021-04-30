b1b9dcfab763da943841266a3681eb49f761baa0
two-ticks <adityasiddheshwar.adisid@gmail.com>
2021-04-30 12:17:37 +0530

    finalise maglev

--- maglev-PID/IE474_O1_Task1_simulink_4/driver.m
+++ maglev-PID/IE474_O1_Task1_simulink_4/driver.m
@@ -1,11 +1,26 @@
+clc;
+clear all;
+close all;
+
 X = 4;                                % group 4
 K = 1;
 a = 0.2 + 0.025 * X * 5;
 b = 100 + 0.5 * X * 5;
 yfinal = 1;                           % by final value theorem  
-sim('IE474_O1_Task1_simulink_4.mdl')
-S = stepinfo(yout,tout,yfinal);
+
+sim('IE474_O1_Task1_simulink_4.mdl')  % simulate
+S = stepinfo(yout,tout,yfinal);       % step response characteristics
+
+% plotting
 figure, plot(tout, yout)
 grid
-disp(S.Overshoot);
-disp(S.SettlingTime);
+axis([-0.01, 0.09, 1.002, 1.014]);
+
+fprintf("Percentage Overshoot %d \n",S.Overshoot);
+fprintf("Settling Time %d \n",S.SettlingTime);
+
+% Routh Hurwitz
+% s^3 + 7570K s^2 + (837999K - 3920.0121)s + 582890K = 0 
+% for stability Inner Product > Outer Product in third order CE
+% therefore,  7570K(837999K - 3920.0121) > 582890 
+% => K > 0.0048 for stable system
--- maglev-PID/IE474_O1_Task2_simulink_4/driver.m
+++ maglev-PID/IE474_O1_Task2_simulink_4/driver.m
@@ -1,24 +1,35 @@
-close all;                         % k > 0.0048
+clear all;
+close all;                           % k > 0.0048 
 clc;
 X      = 4;
 a_vec  = 0.2 + 0.025 * X * (1:10);   % vector including all possible a values 
 b_vec  = 100 + 0.5 * X * (1:10);     % vector including all possible a values 
 yfinal = 0.1;                        % final value that you have calculated in (a) using the final value theorem. 
-K_vec  = 1+0.0048*(1:10);      
+K_vec  = ones(1:10);      
 for ii=1:length(K_vec)
     for jj=1:length(K_vec)
-     a = a_vec(jj);                  
-     b = b_vec(ii);
-     K = K_vec(jj);
-     sim('IE474_O1_Task2_simulink_4.mdl') % You simulate vector including the
-     S = stepinfo(yout, tout, yfinal);
-     PercentOveshoot_matrix(ii,jj) = S.Overshoot;
-     SettlingTime_matrix(ii,jj) = S.SettlingTime;
+         a = a_vec(jj);                  
+         b = b_vec(ii);
+         K = K_vec(jj);
+         sim('IE474_O1_Task2_simulink_4.mdl') % simulate
+         S = stepinfo(yout, tout, yfinal);
+         PercentOveshoot_matrix(ii,jj) = S.Overshoot;
+         SettlingTime_matrix(ii,jj) = S.SettlingTime;
     end
 end
 
-% find max overshoot and minimum settling time
-figure
-%mesh(a_vec, b_vec,PercentOveshoot_matrix(1:10,1:10));
-mesh(a_vec, b_vec,SettlingTime_matrix(1:10,1:10));
+% plotting
+subplot(2,1,1);
+mesh(a_vec, b_vec,PercentOveshoot_matrix);
+title('Percent Oveshoot');
 
+subplot(2,1,2); 
+mesh(a_vec, b_vec,SettlingTime_matrix);
+title('Settling Time');
+
+% find max overshoot and minimum settling time
+max_matrix = (PercentOveshoot_matrix-SettlingTime_matrix); % finding element having least settling time and maximum percentage oveshoot
+[row, col] = find(max_matrix == max(max_matrix(:)));
+
+fprintf('Optimum a value = %d\n', a_vec(col));
+fprintf('Optimum b value = %d\n', b_vec(row));
